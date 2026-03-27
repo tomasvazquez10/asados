0c4d0253156616205ca7b239491fb574d04df44e
tvazquez <tomas.vazquez@besysoft.com>
2026-03-27 15:04:32 -0300

    Saco controller de Usuario

--- src/main/java/com/example/asados/config/CorsConfig.java
+++ src/main/java/com/example/asados/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:8080",
-                "https://asados-1.onrender.com/"
+                "https://asados-1.onrender.com/",
+                "https://asados-frontend.vercel.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
